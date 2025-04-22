5096d00915d28e2d2755ece18f9881d3d083c86c
akshatverma1 <akshaverma14@gmail.com>
2025-04-22 19:13:12 +0530

    Basic Backend

---
+++ Backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+const mainPort = process.env.PORT;
+app.listen(mainPort,()=>{
+    console.log("Server is Running On = "+mainPort);
+})
+
+app.get("/",(req,res)=>{
+    res.send("Home Page");
+})
